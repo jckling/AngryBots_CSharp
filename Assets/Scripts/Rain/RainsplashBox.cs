5036272a12dd1d215ee4c6cf81c73b706b29c224
jckling <jckling@163.com>
2023-04-20 15:17:48 +0800

    bake light; fix splash effect

--- Assets/Scripts/Rain/RainsplashBox.cs
+++ Assets/Scripts/Rain/RainsplashBox.cs
@@ -7,6 +7,20 @@ public class RainsplashBox : MonoBehaviour
 
     private RainsplashManager manager;
 
+    private void Start()
+    {
+        transform.localRotation = Quaternion.identity;
+
+        manager = transform.parent.GetComponent<RainsplashManager>();
+        bounds = new Bounds(new Vector3(transform.position.x, 0.0f, transform.position.z),
+                             new Vector3(manager.areaSize, Mathf.Max(manager.areaSize, manager.areaHeight), manager.areaSize));
+
+        mf = GetComponent<MeshFilter>();
+        mf.sharedMesh = manager.GetPreGennedMesh();
+
+        enabled = false;
+    }
+
     private void OnBecameVisible()
     {
         enabled = true;
